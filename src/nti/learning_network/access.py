f9a8b379d0362fdcf456282a47d2ae1a0e40e866
Josh Zuech <josh.zuech@nextthought.com>
2015-04-23 20:40:59 +0000

    Checkpoint access zones db work
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.learning_network/trunk@63766 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/learning_network/access.py
+++ src/nti/learning_network/access.py
@@ -10,20 +10,59 @@ logger = __import__('logging').getLogger(__name__)
 
 from zope import component
 
+from nti.analytics.recorded.interfaces import VideoWatchRecordedEvent
+from nti.analytics.recorded.interfaces import TopicViewedRecordedEvent
+from nti.analytics.recorded.interfaces import ResourceViewedRecordedEvent
 from nti.analytics.recorded.interfaces import IAnalyticsSessionRecordedEvent
 
-from .database.access import update_access_zones
-from .database.access import update_platform_access
+from nti.ntiids.ntiids import find_object_with_ntiid
+
 from .database.access import get_platform_stats
+from .database.access import update_video_access
+from .database.access import update_reading_access
+from .database.access import update_platform_access
+from .database.access import update_assessment_access
+from .database.access import update_assignment_access
+from .database.access import update_discussion_access
 
 get_platform_stats = get_platform_stats
 
-# def update_access_zones( user, timestamp, duration, course=None ):
-# def update_platform_access( user, timestamp, duration ):
+def _update_access( to_call, event, duration ):
+	if duration > 0:
+		to_call( event.user, event.timestamp, duration )
 
 @component.adapter( IAnalyticsSessionRecordedEvent )
 def _analytics_session( session ):
 	duration = session.SessionEndTime - session.SessionStartTime
 	duration = duration.seconds
-	if duration > 0:
-		update_platform_access( session.user, session.timestamp, duration )
+	_update_access( update_platform_access, session, duration )
+
+@component.adapter( VideoWatchRecordedEvent )
+def _video_view( event ):
+	_update_access( update_video_access, event, event.duration )
+
+@component.adapter( TopicViewedRecordedEvent )
+def _discussion_view( event ):
+	_update_access( update_discussion_access, event, event.duration )
+
+@component.adapter( ResourceViewedRecordedEvent )
+def _reading_view( event ):
+	# FIXME We need to know if we have assessment/assignment/reading/external
+	# probably distinct events
+	#from IPython.core.debugger import Tracer;Tracer()()
+	_update_access( update_reading_access, event, event.duration )
+
+# FIXME
+@component.adapter( ResourceViewedRecordedEvent )
+def _assessment_view( event ):
+	pass
+# 	from IPython.core.debugger import Tracer;Tracer()()
+# 	_update_access( update_assessment_access, event, event.duration )
+
+# FIXME
+@component.adapter( ResourceViewedRecordedEvent )
+def _assignment_view( event ):
+	pass
+# 	from IPython.core.debugger import Tracer;Tracer()()
+# 	_update_access( update_assignment_access, event, event.duration )
+
--- src/nti/learning_network/database/access.py
+++ src/nti/learning_network/database/access.py
@@ -10,7 +10,6 @@ logger = __import__('logging').getLogger(__name__)
 
 from sqlalchemy import Column
 from sqlalchemy import Integer
-
 from sqlalchemy.schema import Sequence
 
 from . import Base
@@ -25,10 +24,37 @@ from .meta_mixins import StatMixin
 from .meta_mixins import LearningNetworkTableMixin
 from .meta_mixins import CourseLearningNetworkTableMixin
 
-class LearningAccessZones( Base, CourseLearningNetworkTableMixin, StatMixin ):
-	__tablename__ = 'LearningAccessZones'
+class _LearningAccessZones( CourseLearningNetworkTableMixin, StatMixin ):
+	pass
 
-	access_zones_id = Column('access_zones_id', Integer, Sequence( 'access_zones_seq' ),
+class DiscussionAccess( Base, _LearningAccessZones ):
+	__tablename__ = 'DiscussionAccess'
+
+	discussion_access_id = Column('discussion_access_id', Integer, Sequence( 'discussion_access_seq' ),
+							index=True, nullable=False, primary_key=True )
+
+class VideoAccess( Base, _LearningAccessZones ):
+	__tablename__ = 'VideoAccess'
+
+	video_access_id = Column('video_access_id', Integer, Sequence( 'video_access_seq' ),
+							index=True, nullable=False, primary_key=True )
+
+class ReadingAccess( Base, _LearningAccessZones ):
+	__tablename__ = 'ReadingAccess'
+
+	reading_access_id = Column('reading_access_id', Integer, Sequence( 'reading_access_seq' ),
+							index=True, nullable=False, primary_key=True )
+
+class SelfAssessmentAccess( Base, _LearningAccessZones ):
+	__tablename__ = 'SelfAssessmentAccess'
+
+	assessment_access_id = Column('assessment_access_id', Integer, Sequence( 'assessment_access_seq' ),
+							index=True, nullable=False, primary_key=True )
+
+class AssignmentAccess( Base, _LearningAccessZones ):
+	__tablename__ = 'AssignmentAccess'
+
+	assignment_access_id = Column('assignment_access_id', Integer, Sequence( 'discussion_access_seq' ),
 							index=True, nullable=False, primary_key=True )
 
 class PlatformAccess( Base, LearningNetworkTableMixin, StatMixin ):
@@ -37,12 +63,27 @@ class PlatformAccess( Base, LearningNetworkTableMixin, StatMixin ):
 	platform_access_id = Column('platform_access_id', Integer, Sequence( 'platform_access_seq' ),
 							index=True, nullable=False, primary_key=True )
 
-def update_access_zones( user, timestamp, duration, course=None ):
+def _update_access_for_table( table, user, timestamp, duration, course=None ):
 	bucket_record = get_course_bucket_for_timestamp(
-							LearningAccessZones, timestamp,
+							table, timestamp,
 							user, course, create=True )
 	bucket_record.last_modified = timestamp
 	update_record_stats( bucket_record, duration )
+
+def update_discussion_access( user, timestamp, duration, course=None ):
+	_update_access_for_table( DiscussionAccess, user, timestamp, duration, course=course )
+
+def update_video_access( user, timestamp, duration, course=None ):
+	_update_access_for_table( VideoAccess, user, timestamp, duration, course=course )
+
+def update_reading_access( user, timestamp, duration, course=None ):
+	_update_access_for_table( ReadingAccess, user, timestamp, duration, course=course )
+
+def update_assessment_access( user, timestamp, duration, course=None ):
+	_update_access_for_table( SelfAssessmentAccess, user, timestamp, duration, course=course )
+
+def update_assignment_access( user, timestamp, duration, course=None ):
+	_update_access_for_table( AssignmentAccess, user, timestamp, duration, course=course )
 
 def update_platform_access( user, timestamp, duration ):
 	bucket_record = get_bucket_for_timestamp( PlatformAccess, timestamp,
