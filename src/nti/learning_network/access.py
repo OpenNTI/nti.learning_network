2ba8cbce2eb0b52302d4b338a398a50dea98e543
Carlos Sanchez <papachoco@gmail.com>
2017-11-19 18:55:00 -0600

    pep8; pylint

--- src/nti/learning_network/access.py
+++ src/nti/learning_network/access.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*
 """
-.. $Id: access.py 84008 2016-03-04 01:39:05Z carlos.sanchez $
+.. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
-__docformat__ = "restructuredtext en"
-
-logger = __import__('logging').getLogger(__name__)
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
 from zope import interface
 
 from nti.learning_network.interfaces import IAccessScoreProvider
 
+logger = __import__('logging').getLogger(__name__)
+
 
 @interface.implementer(IAccessScoreProvider)
 class _AccessScoreProvider(object):
 
-	def __init__(self, user):
-		self.user = user
+    def __init__(self, user):
+        self.user = user
 
-	def get_score(self, course=None, timestamp=None):
-		pass
+    def get_score(self, course=None, timestamp=None):
+        pass
